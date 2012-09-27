295c04aa264bdbeeafb5bf3c1d69565e1f55665d
David Xia <david@davidxia.com>
2012-09-27 13:56:30 -0400

    Added my custom autocomplete and options plugins

---
+++ custom/plugins/dx-autocomplete/dx-autocomplete.plugin.zsh
@@ -0,0 +1,9 @@
+################################
+# Completion Styles
+################################
+
+# list of completers to use
+zstyle ':completion:*::::' completer _expand _complete _ignored _approximate
+# allow one error for every three characters typed in approximate completer
+zstyle -e ':completion:*:approximate:*' max-errors \
+    'reply=( $(( ($#PREFIX+$#SUFFIX)/2 )) numeric )'
---
+++ custom/plugins/dx-options/dx-options.plugin.zsh
@@ -0,0 +1,44 @@
+###########################################
+# Zsh options
+# see http://linux.die.net/man/1/zshoptions
+###########################################
+
+# History
+setopt INC_APPEND_HISTORY              # Append incrementally to history file, doesn't overwrite or wait for shell to exit
+setopt SHARE_HISTORY                   # Imports new commands from history file, typed commands are appended to history
+setopt EXTENDED_HISTORY                # Save beginning and ending timestamps to the history file
+setopt HIST_IGNORE_ALL_DUPS            # If new command added to history duplicates older one, older command is removed
+setopt HIST_SAVE_NO_DUPS               # When writing out the history file, older commands that duplicate newer ones are omitted
+setopt HIST_FIND_NO_DUPS               # When searching history in line editor, don't display duplicates of previously found line, even if not contiguous
+
+# Command correction
+setopt CORRECT                         # Try to correct the spelling of commands
+setopt CORRECT_ALL                     # Try to correct the spelling of all arguments in a line
+setopt COMPLETE_IN_WORD                # Complete commands anywhere in the word
+setopt REC_EXACT                       # In completion, recognize exact matches even if they are ambiguous
+
+# Navigation
+setopt AUTO_CD                         # If command issued can't be executed and is name of dir, cd to that dir
+setopt AUTO_LIST                       # Automatically list choices on an ambiguous completion
+
+# Jobs
+setopt LONG_LIST_JOBS                  # List jobs in the long format by default
+
+
+################################
+# Environmental variables
+################################
+
+setopt ALL_EXPORT
+TZ="America/New_York"
+HISTFILE=$HOME/.zhistory
+HISTSIZE=1000
+SAVEHIST=1000
+HOSTNAME="`hostname`"
+PAGER='less'
+EDITOR='vim'
+BROWSER='chrome'
+LC_ALL='en_US.UTF-8'
+LANG='en_US.UTF-8'
+LC_CTYPE=C
+unsetopt ALL_EXPORT
